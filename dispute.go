3f6b6248ad2416195c3bf9bcc9c2e6c025bfc42e
Guillaume Merindol <guillaume@processout.com>
2017-12-27 15:10:46 +0100

    Transaction: adjust dispute fields

--- dispute.go
+++ dispute.go
@@ -5,21 +5,20 @@ import (
 	"time"
 )
 
+// Dispute object for a *Transaction*
 type Dispute struct {
-	XMLName            xml.Name           `xml:"dispute"`
-	Id                 string             `xml:"id"`
-	Amount             *Decimal           `xml:"amount"`
-	AmountDisputed     *Decimal           `xml:"amount-disputed"`
-	CurrencyISOCode    string             `xml:"currency-iso-code"`
-	AmountWon          *Decimal           `xml:"amount-won"`
-	CreatedAt          *time.Time         `xml:"created-at"`
-	DateWon            *time.Time         `xml:"date-won"`
-	Kind               string             `xml:"kind"`
-	Reason             string             `xml:"reason"`
-	TransactionDetails TransactionDetails `xml:"transaction-details"`
+	XMLName xml.Name `xml:"dispute"`
 
-	Status        string                `xml:"status"`
-	StatusHistory *DisputeStatusHistory `xml:"status-history"`
+	Id              string     `xml:"id"`
+	Amount          *Decimal   `xml:"amount"`
+	CurrencyISOCode string     `xml:"currency-iso-code"`
+	AmountWon       *Decimal   `xml:"amount-won"`
+	ReceivedDate    *time.Time `xml:"received-date"`
+	DateWon         *time.Time `xml:"date-won"`
+	Kind            string     `xml:"kind"`
+	Reason          string     `xml:"reason"`
+
+	Status string `xml:"status"`
 }
 
 type TransactionDetails struct {
