660dd06f7c9cd6126c982d359d33f218f76e6264
Guillaume Merindol <guillaume@processout.com>
2017-12-27 15:23:50 +0100

    Transaction: fix xml issues with disputes

--- dispute.go
+++ dispute.go
@@ -1,14 +1,11 @@
 package braintree
 
 import (
-	"encoding/xml"
 	"time"
 )
 
 // Dispute object for a *Transaction*
 type Dispute struct {
-	XMLName xml.Name `xml:"dispute"`
-
 	Id              string     `xml:"id"`
 	Amount          *Decimal   `xml:"amount"`
 	CurrencyISOCode string     `xml:"currency-iso-code"`
